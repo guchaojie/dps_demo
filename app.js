bf833affcb22e6235a240bf91930b9820fbcf180
Gu Chaojie <chao.jie.gu@intel.com>
2017-05-15 22:06:39 +0800

    loop to show toplogy picture in web

--- app.js
+++ app.js
@@ -3,9 +3,29 @@ var express=require('express'),
     fs = require('fs'),
     path = require('path');
     exec = require('child_process').exec;
-    sqlite3 = require('sqlite3');
+    spawn = require('child_process').spawn;
 
+var index = 0;
 
+function CreateImage() {
+    console.log("create image now!");
+    setTimeout(function(){
+     exec('./script/dps_graph.pl | dot -Tpng -o ./public/images/graph.png',
+           {'cwd':'/home/guchaojie/work/server','uid':1000,'gid':1000},
+           function (error, stdout, stderr) {
+              console.log('stdout: ' + stdout);
+	          console.log('stderr: ' + stderr);
+	          if (error !== null) {
+	             console.log('exec error: ' + error);
+			  }
+              index++;
+              setTimeout(run, 1000);
+	   });
+
+   
+    }, 1000);
+    
+}
 
 var app = express();
 
@@ -17,5 +37,26 @@ app.get("/", function(req, res) {
   console.log("New visiter");
   res.sendFile(__dirname + "/public/home.html");
 });
+
+
+function run() {
+    sh  = spawn('sh', ['./script/tree-'+ index%7], {'uid':1000,'gid':1000});
+
+    sh.stdout.on('data', function (data) {
+      console.log('stdout: ' + data);
+    });
+
+    sh.stderr.on('data', function (data) {
+      console.log('stderr: ' + data);
+    });
+
+    sh.on('exit', function (code) {
+      console.log('/script/tree-'+ index%7 + ' exited with code ' + code);
+	  CreateImage();
+    });
+
+}
+
+setTimeout(run, 2000);
 
 module.exports = app;
