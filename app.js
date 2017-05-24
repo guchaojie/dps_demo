c724688b5fd4d19f2b41dab663e128d9350a37e6
Gu Chaojie <chao.jie.gu@intel.com>
2017-05-24 14:43:51 +0800

    add file part to get size
    Signed-off-by: Gu Chaojie <chao.jie.gu@intel.com>

--- app.js
+++ app.js
@@ -11,8 +11,16 @@ var index = 0;
 var imagetable = new HashTable();
 imagetable.put(index, 'graph.png');
 
+function LightTopology(index) {
+    var result = spawn_sync('sh', ['./script/pub-'+ index%7], {'uid':1000,'gid':1000});
+    console.log(result.stdout.toString());
+    console.log(result.stderr.toString());
+    if(result.status){
+        console.log('Publish failure.');
+    }
+}
 
-function CreateImage() {
+function CreateImage(index) {
     console.log("create image now!");
     setTimeout(function(){
     exec('./script/dps_graph.pl | dot -Tpng -o ./public/images/graph.png',
@@ -24,17 +32,17 @@ function CreateImage() {
 	          console.log('exec error: ' + error);
                   return;
 	      }
-             var result = spawn_sync('sh', ['./script/pub-'+ index%7], {'uid':1000,'gid':1000});
-             console.log(result.stdout.toString());
-             console.log(result.stderr.toString());
-             if(result.status){
-                console.log('Publish failure.');
-             }
-              index++;
-              setTimeout(run, 1000);
-	   });
+       });
 
-   
+       fs.stat('./public/images/graph.png', function (err, stats) {
+           if (err) {
+	       console.log("read file information failed");
+	   }
+       
+           console.log('file size is ', stats.size);
+       
+       })
+
     }, 1000);
     
 }
@@ -63,13 +71,16 @@ function run() {
     });
 
     sh.on('exit', function (code) {
-      console.log('/script/tree-'+ index%7 + ' exited with code ' + code);
-	  CreateImage();
+        console.log('/script/tree-'+ index%7 + ' exited with code ' + code);
+	CreateImage(index);
+	LightTopology(index);
+	index++;
+        setTimeout(run, 1000);
     });
  
     
 }
 
-setTimeout(run, 2000);
+setTimeout(run, 1000);
 
 module.exports = app;
